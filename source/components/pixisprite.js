bf3b27c3d1d42aec98f71864fe2c156a404837c2
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2014-06-03 19:15:12 +0530

    Made blank texture a shared resource.
    Had to make it a bit hackey, as RenderTextures cannot be create before the Pixi renderer has been.

--- source/components/pixisprite.js
+++ source/components/pixisprite.js
@@ -1,11 +1,19 @@
 (function () {
 
+    // BlankTexture need not be recreated every time.
+    // But PIXI.RenderTextures cannot be created before a PIXI renderer is initialized.
+    // So we have to monkeypatch this. :[
+    var blankTexture = undefined;
+
     Crafty.c(
         "PixiSprite",
         {
             init: function() {
 
-                var blankTexture = new PIXI.RenderTexture(0, 0);
+                // Create blank texture only if it hasn't already been created.
+                if ( !blankTexture ) {
+                    blankTexture = new PIXI.RenderTexture(0, 0);
+                }
 
                 this.PixiSprite = new PIXI.Sprite(blankTexture);
                 this.PixiSprite.anchor.x = 0.5;
