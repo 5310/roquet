cac5e892cddd3ac116885a8921f794391df28b9c
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2014-06-14 22:22:58 +0530

    Putting, turn-tracking implemented.
    Also moved over the team-enums.

--- source/systems/court.js
+++ source/systems/court.js
@@ -9,81 +9,143 @@
             maxPuttForce: 0.002,
             maxPuttLength: 200,
 
-            paused: false,
-            turnLimit: 2000,
-            turnTime: 0,
+            teams: {
+                NONE: 0,
+                QUAD: 1,
+                TRI: 2,
+                STAR: 3,
+                HEX: 4
+            },
+            playingTeams: [],
 
-            init: function () {
+            paused: false,
+            turnLimit: 200,
+            turnTime: 0,
+            turnTeamIndex: 0,
+
+            init: function ( playingTeams ) {
+
+                Crafty.bind("PixiEnterFrame", Crafty.COURT._tick);
+                Crafty.bind("HammerDoubleTap", Crafty.COURT._doubleTap);
                 Crafty.bind("HammerHoldStart", Crafty.COURT._holdStart);
                 Crafty.bind("HammerHoldEnd", Crafty.COURT._holdEnd);
+
+                for ( var i in playingTeams ) {
+                    Crafty.COURT.playingTeams[i] = {
+                        team: playingTeams[i],
+                        putts: 1,
+                        goals: undefined,
+                        score: undefined
+                    };
+                }
+
             },
 
-            tick: function(flag) {
-                //TODO: Propagate pause-states to physics, rendering, etc. as needed.
-                //TODO: And invoke the pause-screen, etc.
+            _tick: function() {
+                if ( Crafty.COURT.paused ) {
+                    Crafty.PHYSICSSIMULATOR.paused = true; // Play physics.
+                    //TODO: Show the pause-overlay.
+                    //TODO: Update the pause-overlay.
+                } else {
+                    Crafty.PHYSICSSIMULATOR.paused = false; // Pause physics.
+                    //TODO: Hide the pause-overlay.
+                    if ( Crafty.COURT.turnTime <= 0 ) { // If turn timer has run dry.
+                        Crafty.COURT.turnTime = Crafty.COURT.turnLimit; // Reset timer.
+                        Crafty.COURT.turnTeamIndex++; Crafty.COURT.turnTeamIndex %= Crafty.COURT.playingTeams.length; // Increment team tracker.
+                        Crafty.COURT.playingTeams.putts = 1; // Reset team's available putt count.
+                        Crafty.COURT.paused = true; // Pause again.
+                        console.log("Current teamIndex: "+Crafty.COURT.turnTeamIndex); //NOTE:
+                    } else {
+                        Crafty.COURT.turnTime--; // Reduce current turn's time.
+                    }
+                }
+            },
+
+            _doubleTap: function(data) {
+                Crafty.COURT.paused = !Crafty.COURT.paused;
             },
 
             _holdStart: function(data) {
 
-                var bodies = Crafty.PHYSICSSIMULATOR.hitTest(data.point);
+                // Check if current team has any putts left at all before working.
+                if ( Crafty.COURT.playingTeams[Crafty.COURT.turnTeamIndex].putts > 0 ) {
 
-                Crafty.COURT._pullBall = undefined;
-                for (var i = 0; i < bodies.length; i++ ) { // Look for first valid target and save it.
-                    var body = bodies[i];
-                    if ( body.entity.Ball ) {
-                        //TODO: Do game-rule-based validity check for ball.
-                        Crafty.COURT._pullBall = body.entity;
-                        break;
+                    var bodies = Crafty.PHYSICSSIMULATOR.hitTest(data.point);
+
+                    Crafty.COURT._pullBall = undefined;
+                    for (var i = 0; i < bodies.length; i++ ) { // Look for first valid target and save it.
+                        var body = bodies[i];
+                        if ( body.entity.Ball ) {
+                            // Check if a valid ball before registering as target.
+                            if ( body.entity.Ball && body.entity.Ball.team == Crafty.COURT.playingTeams[Crafty.COURT.turnTeamIndex].team ) {
+                                Crafty.COURT._pullBall = body.entity;
+                                break;
+                            }
+                        }
                     }
-                }
 
-                if ( !Crafty.COURT._pullBall ) { // Save pull coordinates if no valid target under cursor.
-                    Crafty.COURT.paused = false; // Unpause when attempting a pull, as per design.
-                    Crafty.COURT._pushPoint.set(data.point.x, data.point.y);
+                    if ( !Crafty.COURT._pullBall ) { // Save pull coordinates if no valid target under cursor.
+                        Crafty.COURT._pushPoint.set(data.point.x, data.point.y);
+                    }
+
                 }
 
             },
             _holdEnd: function(data) {
 
-                var bodies = Crafty.PHYSICSSIMULATOR.hitTest(data.point);
-
-                var maxForce = Crafty.COURT.maxPuttForce;
-                var maxLength = Crafty.COURT.maxPuttLength;
-
-                if (Crafty.COURT._pullBall) { // If there is a pull target, pull it.
-
-                    var force = new Physics.vector(data.point.x, data.point.y);
-                    force.vsub(Crafty.COURT._pullBall.PhysicsBody.state.pos);
-                    var length = force.norm();
-                    force.normalize().mult((force.norm() > maxLength ? maxForce : maxForce*length/maxLength));
-
-                    Crafty.COURT._pullBall.PhysicsBody.applyForce(force);
-
-                } else { // Otherwise, see if there's a push.
+                // Check if current team has any putts left at all before working.
+                if ( Crafty.COURT.playingTeams[Crafty.COURT.turnTeamIndex].putts > 0 ) {
 
                     var bodies = Crafty.PHYSICSSIMULATOR.hitTest(data.point);
 
-                    var pushBall = undefined;
-                    for (var i = 0; i < bodies.length; i++ ) {
-                        var body = bodies[i];
-                        if ( body.entity.Ball ) {
-                            //TODO: Do game-rule-based validity check for ball.
-                            pushBall = body.entity;
-                            break;
+                    var maxForce = Crafty.COURT.maxPuttForce;
+                    var maxLength = Crafty.COURT.maxPuttLength;
+
+                    if (Crafty.COURT._pullBall) { // If there is a pull target, pull it.
+
+                        // Calculate force.
+                        var force = new Physics.vector(data.point.x, data.point.y);
+                        force.vsub(Crafty.COURT._pullBall.PhysicsBody.state.pos);
+                        var length = force.norm();
+                        force.normalize().mult((force.norm() > maxLength ? maxForce : maxForce*length/maxLength));
+
+                        Crafty.COURT.playingTeams[Crafty.COURT.turnTeamIndex].putts--; // Reduce available putts
+                            console.log(Crafty.COURT.playingTeams[Crafty.COURT.turnTeamIndex].putts);
+                        Crafty.COURT.paused = false; // Unpause if putt valid.
+                        Crafty.COURT._pullBall.PhysicsBody.applyForce(force); // Apply force.
+
+                    } else { // Otherwise, see if there's a push.
+
+                        var bodies = Crafty.PHYSICSSIMULATOR.hitTest(data.point);
+
+                        var pushBall = undefined;
+                        for (var i = 0; i < bodies.length; i++ ) {
+                            var body = bodies[i];
+                            if ( body.entity.Ball ) {
+                                // Check if a valid ball before registering as target.
+                                if ( body.entity.Ball && body.entity.Ball.team == Crafty.COURT.playingTeams[Crafty.COURT.turnTeamIndex].team ) {
+                                    pushBall = body.entity;
+                                    break;
+                                }
+                            }
+                        }
+                        if (pushBall) { // Push if there's a valid target.
+
+                            // Calculate force.
+                            var force = new Physics.vector(Crafty.COURT._pushPoint.x, Crafty.COURT._pushPoint.y);
+                            force.vsub(pushBall.PhysicsBody.state.pos);
+                            var length = force.norm();
+                            force.normalize().mult(-1*(length > maxLength ? maxForce : maxForce*length/maxLength));
+
+
+                            Crafty.COURT.playingTeams[Crafty.COURT.turnTeamIndex].putts--; // Reduce available putts
+                            console.log(Crafty.COURT.playingTeams[Crafty.COURT.turnTeamIndex].putts);
+                            Crafty.COURT.paused = false; // Unpause if putt valid.
+                            pushBall.PhysicsBody.applyForce(force); // Apply force.
+
                         }
                     }
-                    if (pushBall) { // Push if there's a valid target.
 
-                        var force = new Physics.vector(Crafty.COURT._pushPoint.x, Crafty.COURT._pushPoint.y);
-                        force.vsub(pushBall.PhysicsBody.state.pos);
-                        var length = force.norm();
-                        force.normalize().mult(-1*(length > maxLength ? maxForce : maxForce*length/maxLength));
-
-                        pushBall.PhysicsBody.applyForce(force);
-
-                    } else {
-                        Crafty.COURT.paused = true; // Pause if no valid push.
-                    }
                 }
             },
 
