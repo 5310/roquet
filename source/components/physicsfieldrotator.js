ea4cf79c615d22f8e9b3aaf07cb35933bd55a652
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2014-06-19 12:46:03 +0530

    Local rotation mode added.

--- source/components/physicsfieldrotator.js
+++ source/components/physicsfieldrotator.js
@@ -22,7 +22,11 @@
         body.state.vel.mult(this.PhysicsFieldAttractor.friction);
 
         // apply rotational acceleration
-        body.accelerate( acc.rotate(-Math.PI/2).normalize().mult(f) );
+        if ( this.PhysicsFieldRotator.orbital ) { // if field set to orbital rotation, rotate around center of field
+            body.accelerate( acc.rotate(-Math.PI/2).normalize().mult(f) );
+        } else { // if not, just rotate the body
+            body.state.vel.rotate(f*Math.PI*10);
+        }
 
         // apply rotation
         body.entity.PhysicsBodyRotation(body.entity.PhysicsBodyRotation()+f*Math.PI*10);
@@ -41,6 +45,7 @@
                 this.PhysicsFieldRotator.strength = 1; // Positive for clockwise.
                 this.PhysicsFieldRotator.order = -2;
                 this.PhysicsFieldRotator.friction = 1;
+                this.PhysicsFieldRotator.orbital = false;
 
                 this.bind("PhysicsField", field.bind(this));
             },
